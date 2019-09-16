f475095924e4c5aae03eeb3259aab5c86ba62127
cnrd <hello@conradweise.com>
2019-09-16 21:58:35 +0200

    vue port, cursor fix

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-const heading = document.getElementById('heading')
-const picture = document.getElementById('picture')
+const app = new Vue({
+  el: '#app',
+  data: {
+    selection: null,
+    descriptions: {},
+  },
+  computed: {
+    heading: function () {
+      if (this.selection === '') {
+        return 'Concieving biased systems'
+      } else {
+        return `${ this.selection }, 2019`
+      }
+    },
+    description: function () {
+      return this.descriptions[this.selection]
+    }
+  },
+  created: async function () {
+    AFRAME.registerComponent('cursor-listener', {
+      init: function () {
+        this.el.addEventListener('click', function(event) {
+          const id = event.detail.intersection.object.el.id
+          if (app.selection === id) return
+          console.log(event.detail)
+          app.selection = id
+        })
+    
+        /* this.el.addEventListener('mouseleave', (event) => {
+          app.selection = ''
+        }) */
+      }
+    })
 
-function set (work) {
-  if (work === 'reset') {
-    heading.innerText = `Concieving biased systems`
-    picture.src = ''
-  } else {
-    heading.innerText = `Concieving biased systems (${ work })`
-    picture.src = `assets/${ work }.png`
+    const response = await fetch('assets/descriptions.json')
+    this.descriptions = await response.json()
   }
-}
+})
