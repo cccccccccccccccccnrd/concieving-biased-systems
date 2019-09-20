3bfc717d2c59fa68419e1b28b5ede70503ea7f0f
Finn Steffens <Finn@nat079048.nat.FH-Koeln.DE>
2019-09-20 14:58:51 +0200

    added videos

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
 const app = new Vue({
-  el: '#app',
-  data: {
-    selection: null,
-    content: {},
-  },
-  computed: {
-    name: function () {
-      if (this.content[this.selection]) {
-        return this.content[this.selection].name
-      }
+    el: '#app',
+    data: {
+        selection: null,
+        content: {},
     },
-    description: function () {
-      if (this.content[this.selection]) {
-        return this.content[this.selection].description
-      }
-    }
-  },
-  created: async function () {
-    AFRAME.registerComponent('cursor-listener', {
-      init: function () {
-        this.el.addEventListener('click', function(event) {
-          const id = event.detail.intersection.object.el.id
-          if (app.selection === id) return
-          console.log(event.detail)
-          app.selection = id
-        })
-    
-        /* this.el.addEventListener('mouseleave', (event) => {
-          app.selection = ''
-        }) */
-      }
-    })
+    computed: {
+        name: function() {
+            if (this.content[this.selection]) {
+                return this.content[this.selection].name
+            }
+        },
+        description: function() {
+            if (this.content[this.selection]) {
+                return this.content[this.selection].description
+            }
+        }
+    },
+    created: async function() {
+        AFRAME.registerComponent('cursor-listener', {
+            init: function() {
+                this.el.addEventListener('click', function(event) {
+                    const id = event.detail.intersection.object.el.id
+                    if (app.selection === id) return
+                    console.log(event.detail)
+                    app.selection = id
+                })
 
-    const response = await fetch('assets/content.json')
-    this.content = await response.json()
-  }
+                /* this.el.addEventListener('mouseleave', (event) => {
+                  app.selection = ''
+                }) */
+            }
+        })
+
+        const response = await fetch('assets/content.json')
+        this.content = await response.json()
+    },
+    mounted: function() {
+        const manifestoVideo = this.$refs.manifestoAsset3
+        manifestoVideo.play()
+
+        setTimeout(() => {
+            const manifestoVideo = this.$refs.manifestoAsset2
+            console.log(manifestoVideo)
+            manifestoVideo.play()
+        }, 23.7 * 1000)
+
+        setTimeout(() => {
+            const manifestoVideo = this.$refs.manifestoAsset1
+            console.log(manifestoVideo)
+            manifestoVideo.play()
+        }, 47.4 * 1000)
+    }
 })
